5e1514aa22d7285419837bc0f6299d3a26c77682
Logan Fagg <logan.fagg@ibexdevelopment.com>
2017-01-15 13:08:26 -0600

    Added main server script

---
+++ server/pyChatServer.py
@@ -0,0 +1,14 @@
+#pyChatServer.py
+#loganfa 15 Jan 2017
+#
+#Runs server instance
+
+ver = '2017.01.15.0'
+
+print('Loading pyChat Server '+ ver)
+
+#import statements
+
+import socket #UDP library for python
+#import netifaces as ni #network Interfaces for python
+
